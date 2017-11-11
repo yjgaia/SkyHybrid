23e9e42fc9e57bfa8e48f21168f886a40affe89b
Hanul <hanul@hanul.me>
2017-11-11 22:11:52 +0900

    푸시 기능 제작중

---
+++ android/app/src/main/java/co/hanul/hybridapp/push/MyFirebaseInstanceIDService.java
@@ -0,0 +1,6 @@
+package co.hanul.hybridapp.push;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
---
+++ android/app/src/main/java/co/hanul/hybridapp/push/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package co.hanul.hybridapp.push;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
