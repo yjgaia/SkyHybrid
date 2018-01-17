d554373ee45153801477998874afd73a3d075d80
YJ <hanul@hanul.me>
2018-01-18 00:08:28 +0900

    버그 수정

--- android/app/src/main/java/co/hanul/hybridapp/TvActivity.java
+++ android/app/src/main/java/co/hanul/hybridapp/TvActivity.java
@@ -1,16 +1,3 @@
 package co.hanul.hybridapp;
 
-import android.os.Bundle;
-
-import com.btncafe.AsteroidGirl.R;
-
-public class TvActivity extends MainActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        Settings.mainViewId = R.layout.activity_tv;
-
-        super.onCreate(savedInstanceState);
-    }
-}
+public class TvActivity extends MainActivity {}
